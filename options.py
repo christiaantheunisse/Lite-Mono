0634a09568a026aaa365e6dd368ca4ec83d4d3ec
Christiaan Theunisse <ctheunisse@tudelft.nl>
2023-04-01 23:21:02 +0200

    small fixes in training procedure for reduced dataset

--- options.py
+++ options.py
@@ -28,7 +28,7 @@ class LiteMonoOptions:
         self.parser.add_argument("--split",
                                  type=str,
                                  help="which training split to use",
-                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark"],
+                                 choices=["eigen_zhou", "eigen_full", "odom", "benchmark", "eigen_reduced"],
                                  default="eigen_zhou")
         self.parser.add_argument("--model",
                                  type=str,
